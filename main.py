65c04f430859e59c5a5ae9eeb7c50195fb8a5efd
dehuy69 <dr.huy69@gmail.com>
2018-10-13 16:42:23 +0700

    fix bug

--- main.py
+++ main.py
@@ -6,19 +6,16 @@ import time
 import os
 import pandas as pd
 
-def say(text, lang='en'):
+def say(visitor):
     ss2 = gTTS(text=visitor.name.get_values()[0], lang='vi')
     with open('name.mp3', 'wb') as f:
         ss2.write_to_fp(f)
     os.system("mpg321 hello.mp3")
+    os.system("mpg321 name.mp3")
     if visitor.gender.get_values()[0] == 'male':
         os.system("mpg321 boy.mp3")
     else:
         os.system("mpg321 girl.mp3")
-    # ss4 = gTTS(text='Nice to meet you, welcomes to G bee english. Please enjoy yourself', lang='en')
-
-    # with open('welcomes.mp3', 'wb') as f:
-    #     ss4.write_to_fp(f)
     os.system("mpg321 welcomes.mp3")
 
 
@@ -43,14 +40,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                     print('Data : ', obj.data, '\n')
                     qr_id = obj.data.decode('utf8')
                     visitor = df.query("qr_id == @qr_id")
-
-                    say('hello')
-                    say(visitor.name.get_values()[0], lang='vi')
-                    if visitor.gender.get_values()[0] == 'male':
-                        say("what's a handsome boy")
-                    else:
-                        say("what's a beautiful girl")
-                    say('Nice to meet you, welcomes to G bee english, please enjoy yourself')
+                    say(visitor)
                     privious_data = obj.data
         rawCapture.truncate(0)
     except:
